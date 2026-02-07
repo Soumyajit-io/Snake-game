26df644e309bd2234d5de4208856c77bb9e61252
Soumyajit-io <soumyajit010805@gmail.com>
2026-02-07 10:06:00 +0530

    Added sound effects and LLM

--- main.py
+++ main.py
@@ -8,7 +8,22 @@ screen_width = 900
 screen_height = 600
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Snake game")
+t2 = threading.Thread(target=snkagent, args=(("Introduce yourself " ,10,0,(45,55),(100,200))))
+t2.start()
 
+# sounds
+# load bg music
+pygame.mixer.music.load(r'Sounds\bg.mp3')
+pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.play(-1,0.0)
+# load sound
+eating_effect = pygame.mixer.Sound(r'Sounds\eating.mp3')
+hiss_effect = pygame.mixer.Sound(r'Sounds\hiss-effect.mp3')
+game_over_effect = pygame.mixer.Sound(r'Sounds\game_over.mp3')
+
+# image
+bg_img = pygame.image.load(r"Image\bg-img.jpg")
+background = pygame.transform.scale(bg_img, (screen_width, screen_height))
 # colors
 bg = (50, 205, 50)
 red = (255,0,0)
@@ -50,8 +65,8 @@ def game_loop():
     snk_length = 10
 
     
-    def snake_ai(evt):
-        if random.randint(0,1):
+    def snake_ai(evt,percent):
+        if random.random()<(percent/100):
             t2 = threading.Thread(target=snkagent, args=((evt,len(snk_list),score,(snake_x,snake_y),(food_x,food_y))))
             t2.start()
 
@@ -65,15 +80,16 @@ def game_loop():
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    snake_ai("Game over")
                     pygame.quit()
                     running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     game_over=False
+                    pygame.mixer.music.play()
                     game_loop()
         else:
             screen.fill(bg)
+            # screen.blit(background,(0,0))
             text(f"Score: {score}",black,40,20,20)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -85,19 +101,26 @@ def game_loop():
                 if keys[pygame.K_SPACE] and len(snk_list)>=11:
                     
                     if velocity_dir=='u':
-                        snake_ai("Started Sprinting")
+                        hiss_effect.play()
+                        snake_ai("Snake started sprinting",10)
                         velocity_y =-2*speed
                         velocity_x =0
                         snk_length = max(10,snk_length-1)
                     if velocity_dir=='d':
+                        hiss_effect.play()
+                        snake_ai("Snake started sprinting",10)
                         velocity_y =2*speed
                         velocity_x =0
                         snk_length = max(10,snk_length-1)
                     if velocity_dir=='l':
+                        hiss_effect.play()
+                        snake_ai("Snake started sprinting",10)
                         velocity_y =0
                         velocity_x =-2*speed
                         snk_length = max(10,snk_length-1)
                     if velocity_dir=='r':
+                        hiss_effect.play()
+                        snake_ai("Snake started sprinting",10)
                         velocity_y =0
                         velocity_x =2*speed
                         snk_length = max(10,snk_length-1)
@@ -149,15 +172,16 @@ def game_loop():
             snake_x+=velocity_x    
             snake_y+=velocity_y 
             # len of snake
-            print(len(snk_list))
+            # print(len(snk_list))
 
             # food collision
             if abs(snake_x-food_x)<11 and abs(snake_y-food_y)<11:
+                eating_effect.play()
                 score +=10
                 food_x=random.randint(20,880)
                 food_y=random.randint(20,580)
                 snk_length+=8
-                snake_ai("Ate food")         
+                snake_ai(" Snake ate food",22)        
             # snake length increment
             snk_list.append([snake_x,snake_y])
             
@@ -167,9 +191,15 @@ def game_loop():
 
             # snake collide itself condition
             if [snake_x,snake_y] in snk_list[:-1]:
+                game_over_effect.play()
+                pygame.mixer.music.pause()
+                snake_ai("Game over, Reason : Snake ate itself due to the user",100)
                 game_over =True
             # snake collide outside the boundary 
             if snake_x< 0 or snake_x > screen_width or snake_y<0 or snake_y>screen_height :
+                game_over_effect.play()
+                pygame.mixer.music.pause()
+                snake_ai("Game over, Reason: Snake goes outside the boundary due to the user",100)
                 game_over =True
 
             # plot snake
