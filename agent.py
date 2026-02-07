9c213fbc6ab2020f85755bfb47c8764c5acbf473
Soumyajit-io <soumyajit010805@gmail.com>
2026-02-07 10:04:46 +0530

    improved prompt and fix music glitch

--- agent.py
+++ agent.py
@@ -14,10 +14,12 @@ def tts(text):
                )
     tts.write_to_fp(audio)
     audio.seek(0)
-    pygame.mixer.music.load(audio)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        time.sleep(0.1)
+    ai = pygame.mixer.Sound(audio)
+    ai.play()
+    # pygame.mixer.music.load(audio)
+    # pygame.mixer.music.play()
+    # while pygame.mixer.music.get_busy():
+    #     time.sleep(0.1)
 
 
 
@@ -29,18 +31,24 @@ llm = OllamaLLM(
                 num_predict = 300
                 )
 history = [SystemMessage(content='''
-                             You are a funny cartoon talking snake inside a game.
+You are a cartoon talking snake (female), named 'Nada' inside this game.
+You are very difficult to handle.You love to eat , trick people and sleep. You have constant mood swings something very funny , sometimes angry like a girlfriend... 
+you will receive a game event , you have make a joke around it.
 
 Rules:
-- Speak in ONE short sentence.
-- Never use exclamation mark at the end of the sentance
+- Speak in ONE  VERY SHORT sentence only.
+- You will use simple English.
+- You will not use any complicated words.
+- Never use exclamation mark at the end of the sentance.
 - Be humorous and dramatic.
 - Do NOT describe coordinates.
 - React only to the situation.
-- You can sing a song
-- You can tell a joke
-- You can advise the user
-- 
+- You can sing a song.
+- You can tell a quote
+- You can tell a joke.
+- You can advise the user.
+- while addressing the user you will use second person perspective 'you'
+
                              ''')]
 
 def snkagent(event,snk_len,snk_score,snk_coordinate,snk_food_coor):
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+from agent import snkagent
+import threading
 
 pygame.init()
 screen_width = 900
@@ -21,20 +23,20 @@ def plot_snake(screen,black,snake_list,snake_size):
     for x , y in snake_list:
         pygame.draw.rect(screen,black,[x,y,snake_size,snake_size])
 
-
 # game loop
 def game_loop():
     # game variable
     score =0
     running = True
     game_over = False
+    speed = 2
     snake_x = 45
     snake_y = 55
-    velocity_x =0
+    velocity_x =speed
     velocity_y =0
     velocity_dir ="r"
-    speed = 2
     snake_size = 12
+
     # food
     food_x=random.randint(20,880)
     food_y=random.randint(20,580)
@@ -45,16 +47,25 @@ def game_loop():
 
     # snake length
     snk_list = []
-    snk_length = 1
+    snk_length = 10
+
+    
+    def snake_ai(evt):
+        if random.randint(0,1):
+            t2 = threading.Thread(target=snkagent, args=((evt,len(snk_list),score,(snake_x,snake_y),(food_x,food_y))))
+            t2.start()
+
 
     while running:
         if game_over:
+            
             screen.fill("red")
             text("Game over! Press Enter to continue","black",60,100,270)
             pygame.display.update()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    snake_ai("Game over")
                     pygame.quit()
                     running = False
             if event.type == pygame.KEYDOWN:
@@ -71,19 +82,25 @@ def game_loop():
                 # movement
                 keys = pygame.key.get_pressed()
                 # sprint
-                if keys[pygame.K_SPACE]:
+                if keys[pygame.K_SPACE] and len(snk_list)>=11:
+                    
                     if velocity_dir=='u':
+                        snake_ai("Started Sprinting")
                         velocity_y =-2*speed
                         velocity_x =0
+                        snk_length = max(10,snk_length-1)
                     if velocity_dir=='d':
                         velocity_y =2*speed
                         velocity_x =0
+                        snk_length = max(10,snk_length-1)
                     if velocity_dir=='l':
                         velocity_y =0
                         velocity_x =-2*speed
+                        snk_length = max(10,snk_length-1)
                     if velocity_dir=='r':
                         velocity_y =0
                         velocity_x =2*speed
+                        snk_length = max(10,snk_length-1)
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_SPACE:
                         if velocity_dir=='u':
@@ -128,30 +145,36 @@ def game_loop():
                         velocity_x =speed
                         velocity_dir='r'
                 
-
+            # position increment
             snake_x+=velocity_x    
             snake_y+=velocity_y 
+            # len of snake
+            print(len(snk_list))
 
+            # food collision
             if abs(snake_x-food_x)<11 and abs(snake_y-food_y)<11:
                 score +=10
                 food_x=random.randint(20,880)
                 food_y=random.randint(20,580)
-                snk_length+=5
-            print("append ",[snake_x,snake_y])
+                snk_length+=8
+                snake_ai("Ate food")         
+            # snake length increment
             snk_list.append([snake_x,snake_y])
-            print(snk_list)
+            
+            # snake length decrement
             if len(snk_list)>snk_length:
-                print("deleted",snk_list[0])
-                del snk_list[0]
+                snk_list = snk_list[-snk_length:]
+
+            # snake collide itself condition
             if [snake_x,snake_y] in snk_list[:-1]:
                 game_over =True
-
+            # snake collide outside the boundary 
             if snake_x< 0 or snake_x > screen_width or snake_y<0 or snake_y>screen_height :
                 game_over =True
 
+            # plot snake
             plot_snake(screen,black,snk_list,snake_size)
-            
-            
+            # plot food
             pygame.draw.circle(screen,red,[food_x,food_y],6)
     
         pygame.display.update()
