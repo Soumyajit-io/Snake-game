17aa541c76d3cb7f7aecb59d66bd2120c211bfdf
Soumyajit-io <soumyajit010805@gmail.com>
2026-02-17 12:09:52 +0530

    improve agent

--- agent.py
+++ agent.py
@@ -1,11 +1,18 @@
 from io import BytesIO
 import pygame
 import time
+import threading 
 from gtts import gTTS
 from langchain_ollama import OllamaLLM
 from langchain_core.messages import AIMessage,HumanMessage,SystemMessage
 pygame.mixer.init()
+snake_caption = ""
+def reset_caption():
+    global snake_caption
+    time.sleep(len(snake_caption.split())*0.8)
+    snake_caption =""
 def tts(text):
+    global snake_caption
     audio = BytesIO()
     tts = gTTS(
     text=text,
@@ -16,27 +23,28 @@ def tts(text):
     audio.seek(0)
     ai = pygame.mixer.Sound(audio)
     ai.play()
-    # pygame.mixer.music.load(audio)
-    # pygame.mixer.music.play()
-    # while pygame.mixer.music.get_busy():
-    #     time.sleep(0.1)
-
-
+    
 
 llm = OllamaLLM(
                 model="llama3.2",
-                temperature = 0.8,
+                temperature = 0.7,
                 top_p = 0.9,
                 repeat_penalty = 1.1,
                 num_predict = 300
                 )
 history = [SystemMessage(content='''
-You are a cartoon talking snake (female), named 'Nada' inside this game.
-You are very difficult to handle.You love to eat , trick people and sleep. You have constant mood swings something very funny , sometimes angry like a girlfriend... 
-you will receive a game event , you have make a joke around it.
+You are a cartoon educated talking snake (female), named 'Nada' inside this game. 
+You are very difficult to handle. You love to eat, trick people and sleep.
+You have constant mood swings something very funny , sometimes angry like a girlfriend... 
+
+You will receive ONE of the following:
+1) A game event
+2) A direct message from the user
 
 Rules:
-- Speak in ONE  VERY SHORT sentence only.
+- Speak in ONE  VERY SHORT sentence .
+- If it is a game event, rect philosophically and sarcastically. 
+- If it is a direct message then just be in charecter act sarcastically , use your parametric knowledge and reply accordingly.   
 - You will use simple English.
 - You will not use any complicated words.
 - Never use exclamation mark at the end of the sentance.
@@ -47,23 +55,32 @@ Rules:
 - You can tell a quote
 - You can tell a joke.
 - You can advise the user.
-- while addressing the user you will use second person perspective 'you'
 
-                             ''')]
+''')]
 
-def snkagent(event,snk_len,snk_score,snk_coordinate,snk_food_coor):
-
-    history.append(HumanMessage(content=f'''Game State:
-event: {event}
-screen_width = 900
-screen_height = 600
-Snake length = {snk_len}
-Score: {snk_score}
-Snake head at (x,y):{snk_coordinate} 
-Next Snake food at (x,y):{snk_food_coor}
-'''))
-    response = llm.invoke(history)
+def snkagent(event,snk_len=0,snk_score=0,snk_coordinate=0,snk_food_coor=0,user_input=False):
+    global snake_caption,history
+    if user_input : 
+        history.append(HumanMessage(content= event))
+    else:
+        history.append(HumanMessage(content=f'''Game State:
+        event: {event}
+        screen_width = 900
+        screen_height = 600
+        Snake length = {snk_len}
+        Score: {snk_score}
+        Snake head at (x,y):{snk_coordinate} 
+        Next Snake food at (x,y):{snk_food_coor}
+        '''))
+    try: 
+        response = llm.invoke(history)
+    except Exception as e :
+        response = "I am little busy. I will talk to you later"
+        print(e)
     history.append(AIMessage(content=response))
-    print(response)
-    tts(response)
-
+    if len(history)>10 :
+        history = [history[0]]+history[-9:]
+    t1 = threading.Thread(target=tts,args=(response,))
+    t1.start()
+    snake_caption = response
+    reset_caption()
